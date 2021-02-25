d1f16b08ac081ac35a5e04806c7e7e9bb5c3b01d
Bernardo Sertório <besertorio@gmail.com>
2021-02-25 17:07:41 -0300

    styled signIn page

--- borabora-web/src/pages/Signin/styles.ts
+++ borabora-web/src/pages/Signin/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets';
+import signInBackgroundImg from '../../assets/sign-in-background.jpg';
 
 export const Container = styled.div`
   height: 100vh;
@@ -12,11 +13,92 @@ export const Container = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-
-  place-content: center;
+  align-items: center;
 
   width: 100%;
   max-width: 700px;
+
+  img {
+    width: 220px;
+    margin-top: 80px;
+  }
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    margin-top: 10px;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #fff;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      width: 100%;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#fff')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #232129;
+    display: block;
+    margin-top: -50px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#232129')};
+    }
+  }
 `;
 
 export const Background = styled.div`
