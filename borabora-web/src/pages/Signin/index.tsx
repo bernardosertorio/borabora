08d0ae79eae2c862975f1818f0b018c96b0bfe47
Bernardo Sertório <besertorio@gmail.com>
2021-02-25 17:50:18 -0300

    fix logo format

--- borabora-web/src/pages/Signin/index.tsx
+++ borabora-web/src/pages/Signin/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
-import logoImg from '../../assets/logo.jpg';
+import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
 
